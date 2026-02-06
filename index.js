82ecb2c7e7526ef7760b99c952316d759868258b
F1lthyAn1mal <ads@adammo.co.uk>
2026-02-06 15:44:44 +0000

    finished unit tests

--- index.js
+++ index.js
@@ -13,20 +13,139 @@
 'use strict';
 
 function id() {
-  // e.g. return "UP654321";
+  return 'UP2277697';
 }
 
 function fn() {
-  return 'Replace This With Your First Name';
+  return 'Adam';
 }
 
 function sn() {
-  return 'Replace This With Your Surname';
+  return 'Morris';
 }
 
-function example() {
-  // replace this example with
-  // your first function then
-  // add more below as necessary.
+function add(a, b ) {
+  return a + b;
+}
 
+function subtract(a, b) {
+  return a - b;
+}
+
+function checkObject(obj) {
+  obj.checked = true;
+}
+
+function checkObjectInside(obj) {
+  //if (typeof obj.data === 'object' && obj.data !== null) {
+  //  obj.data.checked = true;
+  //}
+  if (obj.data) {
+    checkObject(obj.data);
+  }
+}
+
+
+function arraySet(arr, i, n) {
+  if (arr[i] !== null && i < arr.length && Number.isInteger(i)) {
+    arr[i] = n;
+  }
+}
+
+function addAll(arr) {
+  // let total = 0
+  // for (const numb of arr) {
+  //   total += numb;
+  // }
+  // return total;
+  return arr.reduce((sum, n) => sum + n, 0);
+}
+
+function larger(a, b) {
+  // if (a > b) {
+  //   return a;
+  // } else {
+  //   return b;
+  // }
+  return a > b ? a : b;
+}
+
+
+function largest(arr) {
+  //   let max = arr[0];
+  // for (let numb of arr) {
+  //   if (numb > max) {
+  //     max = numb;
+  //   }
+  // }
+  // if (max === undefined) {
+  //   return null;
+  // } else {
+  //   return max;
+  // }
+   return arr.length === 0 ? null : Math.max(...arr);
+}
+
+function compareAttempt(a, b) {
+
+}
+function compare(a, b) {
+    // let ident = true;
+
+  // if (a.length !== b.length) {
+  //   return false;
+  // }
+
+  // for (let i = 0; i<a.length; ++i) {
+  //   if (a[i] !== b[i]) {
+  //     ident = false;
+  //   }
+  // }
+
+  // return ident;
+  // return a.length === b.length && a.every((val, i) => val === b[i]);
+  return a.length === b.length && JSON.stringify(a) === JSON.stringify(b);
+}
+
+
+function addToAll(arr, n) {
+  // for (let i = 0; i < arr.length; i++){
+  //   arr[i] += n;
+  // }
+  // return arr;
+  arr.forEach((_, i) => arr[i] += n);
+  return arr;
+}
+
+let remembered;
+function rememberThis(keepsake) {
+  remembered = keepsake;
+}
+
+function nArray(n) {
+  // const arr = [];
+  // for (let i = 1; i<=n; i++) {
+  //   arr.push(i);
+  // }
+  // return arr;
+  return Array.from({length: n}, (_, index) => index + 1);
+}
+
+function addAllOpt(arr) {
+  // if (arr.length == 0 || !Array.isArray(arr)) {return 0;}
+  // let sum = 0;
+  // for (let i = 0; i<arr.length; i++) {
+  //   sum += arr[i];
+  // }
+  // return sum;
+  if (!arr || !arr.length) return 0;
+  return addAll(arr);
+}
+
+function divisors(arr, div) {
+  return arr.filter((num) => !(num % div));
+}
+
+function multiples(n, m) {
+  return Array.from({length:n}, (_, index) => (index + 1) * m);
 }
